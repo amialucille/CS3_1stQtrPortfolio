12c0dd82ff6beb31bdffd59eec3f65ba4e207d65
amialucille <b2029alqkatigbac@pshs.edu.ph>
2025-08-05 09:07:57 +0800

    commit ng

--- public/script.js
+++ public/script.js
@@ -5,11 +5,4 @@ let maxAge = 100
 let numPerDay = 2
 
 let totalRequired = (numPerDay * 365)*(maxAge - age);
-document.write("You will need" + totalRequired + "to last you until the ripe old age of" + maxAge);
-
-let age = 15
-let maxAge = 100
-let numPerDay = 2
-
-let totalRequired = (numPerDay * 365)*(maxAge - age);
-document.write("You will need" + totalRequired + "to last you until the ripe old age of" + maxAge);
+document.write("You will need " + totalRequired + "to last you until the ripe old age of " + maxAge);
