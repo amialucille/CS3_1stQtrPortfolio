c1565658b665082dc64e29d3bfa06ba8d8edcb39
amialucille <b2029alqkatigbac@pshs.edu.ph>
2025-08-12 08:34:38 +0800

    commit ng

--- public/script.js
+++ public/script.js
@@ -1,8 +1,26 @@
 // no script tag needed
 
+var x = prompt("Enter age", "0");
+var y = prompt("Enter maxAge", "0");
+var z = prompt("Enter amount of times you eat your favorite snack per day" , "0");
+
 let age = 15
 let maxAge = 100
 let numPerDay = 2
 
 let totalRequired = (numPerDay * 365)*(maxAge - age);
-document.write("You will need  " + totalRequired + "to last you until the ripe old age of  " + maxAge);
+
+let age = window.prompt("Enter your age : ", 15)
+let maxAge = window.prompt("You will live until the ripe old age of : ", "100");
+let numPerDay = window.prompt("NUmber of Snacks daily: ", 2)
+
+let sure = window.confirm('Sure of your numbers ${age}, ${maxAge}, ${numperday}')
+if(sure){
+    let totalRequired = (Number(numPerDay)*365) * (Number(maxAge));
+}
+if(confirm('Are you sure?')){
+    document.write("You will need  " + totalRequired + "to last you until the ripe old age of  " + maxAge);
+}
+else{
+    console.log("Then who ????")
+}
